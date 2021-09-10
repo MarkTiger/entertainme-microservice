84fec11d4e8cd35e51a923cfcc48754c06c3c53a
MarkTiger <marco.tiger.98@gmail.com>
2021-09-10 21:05:53 +0700

    GraphQL tv series

--- server/orchestrator/schemas/tvseries.js
+++ server/orchestrator/schemas/tvseries.js
@@ -12,9 +12,23 @@ module.exports = {
       tags: [String]
     }
 
+    input TvInput {
+      title: String!
+      overview: String!
+      poster_path: String!
+      popularity: Float!
+      tags: [String!]!
+    }
+
     extend type Query {
       tvSeries: [TvSeries]
       tvSeriesOne(id: ID!): TvSeries
+    }
+
+    extend type Mutation {
+      createTvSeries(payload: TvInput): String
+      updateTvSeries(payload: TvInput, id: ID!): String
+      deleteTvSeries(id: ID!): String
     }
   `,
   resolvers: {
@@ -32,6 +46,35 @@ module.exports = {
         try {
           const { data } = await tvSeriesAPI.get('/' + args.id);
           return data;
+        } catch (err) {
+          return 'error';
+        }
+      },
+    },
+
+    Mutation: {
+      async createTvSeries(parent, args) {
+        try {
+          const { data } = await tvSeriesAPI.post('/', args);
+          return data.insertedId;
+        } catch (err) {
+          return 'error';
+        }
+      },
+
+      async updateTvSeries(parent, args) {
+        try {
+          await tvSeriesAPI.put('/' + args.id, args);
+          return `Tv Series with id ${args.id} successfully updated`;
+        } catch (err) {
+          return 'error';
+        }
+      },
+
+      async deleteTvSeries(parent, args) {
+        try {
+          await tvSeriesAPI.delete('/' + args.id);
+          return `Tv Series with id ${args.id} successfully deleted`;
         } catch (err) {
           return 'error';
         }
